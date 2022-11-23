cbcbb12b9ba822167ab9547b7c4e4ba89eb86459
ballatae <92359676+ballatae@users.noreply.github.com>
2022-11-23 17:35:45 +0200

    Add files via upload

---
+++ the_bull_gods/app.js
@@ -0,0 +1,64 @@
+// ?nav bar animation
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    burger.addEventListener('click', () => {
+        // ! Toggle nav
+        nav.classList.toggle('nav-active');
+
+        // ? Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;//? the delay of the nav li to show up
+            } 
+        });
+        
+        //?Burger Animation the three lines
+        burger.classList.toggle('toggle');
+    });
+    
+}
+
+navSlide();
+
+// ? welcome text animation
+var welcomeText = document.getElementById("openingText");
+// ? welcome text animation
+var scrollFunction = function () {
+    const scrolled = window.scrollY;
+    welcomeText.style.opacity = 1 + scrolled / -200
+
+    
+}
+
+var slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        // function to change the active images
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) { slideIndex = 1 }
+            if (n < 1) { slideIndex = slides.length }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
